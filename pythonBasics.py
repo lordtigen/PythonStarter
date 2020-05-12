1693f5ca79d9bfb193738028b756ad2cdd7d7283
lordtigen <54910442+lordtigen@users.noreply.github.com>
2020-05-12 22:38:32 +0300

    Update pythonBasics.py
    Saglamlastirildi, hatalar ele alindi

--- pythonBasics.py
+++ pythonBasics.py
@@ -13,13 +13,14 @@ def mukemmelSayiBul():           # Klasik olarak fonksiyonlari yerlestirdim.
         if bolenToplami == girilenSayi:
             print("Bu sayi bir mukemmel sayidir.!!")            
             input("Cikmak icin bir tusa basin!")
-            os.system('cls | | clear')
+            os.system('cls' if os.name == 'nt' else 'clear')
             break
         elif bolenToplami != girilenSayi:
             print("Degildir")
             input("Cikmak icin bir tusa basin!")
+            os.system('cls' if os.name == 'nt' else 'clear')
             break
-            os.system('cls | | clear')      # Cikmak icin bir tusa bastiracagiz ve 
+                                                            # Cikmak icin bir tusa bastiracagiz ve 
                                                              # Terminali temizletecegiz.
 
 def TamBolenleriBul():
@@ -35,9 +36,8 @@ def TamBolenleriBul():
         if bolenler == [1]:                                       # Eger ortak bolen sadece 1 ise. Bu asal sayidir.
             print("Asal sayi secmissin reyis")                    # Birde kendisine bolunebiliyor zaten. Onu saymiyoruz bile
             input("Cikmak icin bir tusa basiniz")
-            break
-            os.system('cls | | clear')
-
+            os.system('cls' if os.name == 'nt' else 'clear')
+        break
             
 def AsalMiBu():
     while secim:
@@ -48,15 +48,14 @@ def AsalMiBu():
                                                            # Kalan eger 0 ise Asal Sayi degildir.
                 print("Senin Sayi asal degil.")
                 input("Cikmak icin bir tusa basin!")
+                os.system('cls' if os.name == 'nt' else 'clear')
                 break
-                os.system('cls | | clear')
-                
+
             else:                                                         
                 print("Senin sayi asal cikti.")
                 input("Cikmak icin bir tusa basin!")
+                os.system('cls' if os.name == 'nt' else 'clear')
                 break
-                os.system('cls | | clear')
-                
         break
 
 def ArmstrgonMuBu():
@@ -71,12 +70,13 @@ def ArmstrgonMuBu():
     if num == sum:
         print(num, "bir Armstrong sayidir")
         input("Cikmak icin bir tusa basin")
-        os.system('cls | | clear')
+        os.system('cls' if os.name == 'nt' else 'clear')
+        
     else:
         print(num, "bir Armstrong sayi degildir")
         input("Cikmak icin bir tusa basin!")
-        os.system('cls | | clear')
-
+        os.system('cls' if os.name == 'nt' else 'clear')
+        
 def SayiyiYaziyaCevir():
     birler = ["","Bir","Iki","Uc","Dort","Bes","Alti","Yedi","Sekiz","Dokuz"]
     onlar = ["","On","Yirmi","Otuz","Kirk","Elli","Altmis","Yetmis","Seksen","Doksan"]
@@ -97,36 +97,40 @@ def CarpimTablosu():
         for j in range(1,11):
             print(f"{i} x {j} = {i*j}")                           # Dongu icinde dongu kullanarak her seferinde
     input("Cikmak icin bir tusa basin!")                          # Donen iki degisken elde ettik.
-    os.system('cls | | clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
+try:
+    while 1:
+
+        print("""
+    [1] Mukemmel Sayiyi Bul
+    [2] Sayinin Tam Bolenlerini Bul
+    [3] Sayi Asal Sayi Mi Bul
+    [4] Armstrong Sayi Mi Bul
+    [5] Yazacagin Sayiyi Yaziya Cevir
+    [6] 1 den 10'a kadar olan sayilari Carpim Tablosuna Bastir
+
+        """)
+        secim = int(input("Bir secim giriniz: "))
+
+        if secim == 1:
+            mukemmelSayiBul()
+        elif secim == 2:
+            TamBolenleriBul()
+        elif secim == 3:
+            AsalMiBu()
+        elif secim == 4:
+            ArmstrgonMuBu()
+        elif secim == 5:
+            SayiyiYaziyaCevir()
+        elif secim == 6:
+            CarpimTablosu()
+            
+        elif secim <= 7:
+            print("6 dan buyuk bir secenek Koydugumu sanmiyorum.")
+            
 
 
-while 1:
-
-    print("""
-[1] Mukemmel Sayiyi Bul
-[2] Sayinin Tam Bolenlerini Bul
-[3] Sayi Asal Sayi Mi Bul
-[4] Armstrong Sayi Mi Bul
-[5] Yazacagin Sayiyi Yaziya Cevir
-[6] 1 den 10'a kadar olan sayilari Carpim Tablosuna Bastir
-
-    """)
-    secim = int(input("Bir secim giriniz: "))
-
-    if secim == 1:
-        mukemmelSayiBul()
-    elif secim == 2:
-        TamBolenleriBul()
-    elif secim == 3:
-        AsalMiBu()
-    elif secim == 4:
-        ArmstrgonMuBu()
-    elif secim == 5:
-        SayiyiYaziyaCevir()
-    elif secim == 6:
-        CarpimTablosu()
-        
-    else:
-        print("Programdan cikiliyor...")
-        quit()
-        os.system('cls' if os.name == 'nt' else 'clear')
+except ValueError:
+    print("1-6 Arasinda bir sayi girmelisin")
+                    
